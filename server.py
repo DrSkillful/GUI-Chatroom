89a3d05b91a5b1fcfcb994af3d05cba5423f883b
DrSkillful <76279774+JohnSkill@users.noreply.github.com>
2022-08-16 15:02:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,52 +1,51 @@
-import threading
 import socket
+import threading
 
-host = "127.0.0.1"  # localhost
-port = 65432
+HOST = '127.0.0.1'
+PORT = 48990
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.bind((HOST, PORT))
+
 server.listen()
 
 clients = []
 nicknames = []
 
-
 def broadcast(message):
     for client in clients:
         client.send(message)
+
 
 def handle(client):
     while True:
         try:
             message = client.recv(1024)
+            print(f'{nicknames[clients.index(client)]} says {message}')
             broadcast(message)
         except:
             index = clients.index(client)
-            client.remove(client)
-            client.close()
+            clients.remove(client)
             nickname = nicknames[index]
-            broadcast(f'{nickname} has left the chatroom'.encode('ascii'))
             nicknames.remove(nickname)
             break
 
 def receive():
     while True:
         client, address = server.accept()
-        print(f'{str(address)} connected')
+        print(f'{str(address)} connected!')
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f'Nickname of this client is {nickname}')
-        broadcast(f'{nickname} has landed in the chatroom'.encode('ascii'))
-        client.send('Connected to the chatroom'. encode("ascii"))
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} has entered the chat!\n'.encode('utf-8'))
+        client.send("Connected to server".encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-
-print('Server is starting...')
-print('Server is now listening...')
+print("Server is running")
 receive()
